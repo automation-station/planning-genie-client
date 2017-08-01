2b711ed6bcef360778e63353d47e0878dc55b707
sdavidson140 <sdavidson140@gmail.com>
2017-08-01 17:10:11 -0400

    dashboard model hook returning current profile
    close #2

--- app/routes/dashboard.js
+++ app/routes/dashboard.js
@@ -1,4 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  auth: Ember.inject.service(),
+  model() {
+    return this.get('store').findRecord('profile', this.get('auth.credentials.profile'))
+  }
 });
