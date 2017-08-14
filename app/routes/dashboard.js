5f3a352693ed96de2ab37caceb6924689be9c1a7
payne-chris-r <06cpayne@gmail.com>
2017-08-14 13:44:15 -0400

    Merging of Scott's solution and mine
    Adds model() hook which grabs the correct profile and then transitions
    to the profile page. And adds `beforeModel` method to route to the
    sign-in page for unauthenticated users. There is definitely a better
    way to do this, but I'm not sure how and this works now.

--- app/routes/dashboard.js
+++ app/routes/dashboard.js
@@ -2,18 +2,16 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   auth: Ember.inject.service(),
-<<<<<<< HEAD
-  model() {
-    return this.get('store').findRecord('profile', this.get('auth.credentials.profile'))
-=======
   profile: Ember.computed.alias('auth.credentials.profile'),
 
+  beforeModel () {
+    if(!this.get('profile')) {
+      this.get('flashMessages')
+        .danger('You must be authenticated to view your dashboard.')
+      this.transitionTo('sign-in')
+    }
+  },
   model () {
-    // this fails if someone just goes to `/dashboard` without logging in
-    // first. Need to figure out how to not load this route if not signed in.
-    // Application route has an error() handler, but it only transitions to
-    // log-in if we get a 401 unauthorized error.
-    // Assertion Failed: `id` passed to `findRecord()` has to be non-empty string or number
     return this.get('store').findRecord('profile', this.get('profile'))
   },
 
@@ -21,6 +19,5 @@ export default Ember.Route.extend({
     dashboard () {
       this.transitionTo('profile.edit', this.get('model'))
     }
->>>>>>> Adds dashboard route and updates nav-bar
   }
 });
