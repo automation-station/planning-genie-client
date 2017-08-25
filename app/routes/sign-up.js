bada20f5332a693461354b6bf49433d8ecdb8729
payne-chris-r <06cpayne@gmail.com>
2017-08-25 13:04:41 -0400

    Adds profile id to the store
    The auth store wasn't being updated previously. Now it is.

--- app/routes/sign-up.js
+++ app/routes/sign-up.js
@@ -3,6 +3,7 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   auth: Ember.inject.service(),
   flashMessages: Ember.inject.service(),
+  profile: Ember.computed.alias('auth.credentials.profile'),
 
   actions: {
     signUp (credentials) {
@@ -14,14 +15,15 @@ export default Ember.Route.extend({
         })
         .then((user) => this.get('store').createRecord('profile', { user }))
         .then((profile) => profile.save())
-        .then((profile) => this.transitionTo('profile', profile))
+        .then((profile) => this.set('auth.credentials.profile', +profile.get('id')))
+        .then(() => this.transitionTo('dashboard'))
         .then(() => {
           this.get('flashMessages')
             .success('Successfully signed-up! You have also been signed-in.');
         })
         .catch(() => {
           this.get('flashMessages')
-            .danger('There was a problem. Please try again.');
+            .danger('There was a problem here. Please try again.');
         });
     },
   },
